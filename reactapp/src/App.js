34cb71e6cf58a8f32e49c0551b4c42c982f6cba6
Tuan Huu Phan <phanhuutuan.01.01.1995@gmail.com>
2018-03-17 06:47:13 +0700

    Add missing file

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, IndexRoute } from "react-router-dom";
+import AdsArea from './components/Ads_Area/AdsArea';
+import Root from './components/Root';
 
 class App extends Component {
   render() {
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Welcome to React</h1>
+    //     </header>
+    //     <p className="App-intro">
+    //       To get started, edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //   </div>
+    // );
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <BrowserRouter>
+      <div>
+          <Route path={"/"} component={Root} />
+          <Route path={"/create/ads-area"} component={AdsArea} />
+          {/* <Route path={"/user/:id"} component={User} />
+          <Route path={"/home"} component={Home} />
+          <Route path={"home-single"} component={Home}/> */}
       </div>
-    );
+      </BrowserRouter>
+  );
   }
 }
 
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import AdsArea from './components/Ads_Area/AdsArea';
+import { BrowserRouter, Link, Route } from "react-router-dom";
 
-ReactDOM.render(<AdsArea />, document.getElementById('root'));
+// const App = () => (
+//     <div>App
+//         <Link> to='/about'>About</Link>
+//     </div>
+// );
+
+// const About = () => (
+//     <div>About
+//         <Link> to='/'>App</Link>
+//     </div>
+// );
+
+// const Main = () => <h1>Hello world</h1>;
+
+
+ReactDOM.render(<App />, window.document.getElementById('root'));
+
+// ReactDOM.render(<BrowserRouter>
+//                     <div>
+//                         <Route path='/' component={Main} />
+//                     </div>
+//                 </BrowserRouter>
+//     , document.getElementById('navigation'));
+// ReactDOM.render(<AdsArea />, document.getElementById('root'));
 registerServiceWorker();
