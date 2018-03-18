e522098f5e73ca29dbcd53844fc378f6d2fd2e69
Phi Nguyen <nnphi219@gmail.com>
2018-03-18 16:26:24 +0700

    add template

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -5,31 +5,7 @@ import './frontend/css/popup.css';
 import './frontend/css/ads_area.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import AdsArea from './components/Ads_Area/AdsArea';
-import { BrowserRouter, Link, Route } from "react-router-dom";
-
-// const App = () => (
-//     <div>App
-//         <Link> to='/about'>About</Link>
-//     </div>
-// );
-
-// const About = () => (
-//     <div>About
-//         <Link> to='/'>App</Link>
-//     </div>
-// );
-
-// const Main = () => <h1>Hello world</h1>;
-
 
 ReactDOM.render(<App />, window.document.getElementById('root'));
 
-// ReactDOM.render(<BrowserRouter>
-//                     <div>
-//                         <Route path='/' component={Main} />
-//                     </div>
-//                 </BrowserRouter>
-//     , document.getElementById('navigation'));
-// ReactDOM.render(<AdsArea />, document.getElementById('root'));
 registerServiceWorker();
