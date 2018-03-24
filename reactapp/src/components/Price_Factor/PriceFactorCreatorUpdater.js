f331aa06753c06c32375f8287e60e420edffdcd7
Phi Nguyen <nnphi219@gmail.com>
2018-03-24 14:54:29 +0700

    change state on popup form

--- reactapp/src/components/Price_Factor/PriceFactorCreatorUpdater.js
+++ reactapp/src/components/Price_Factor/PriceFactorCreatorUpdater.js
@@ -20,13 +20,13 @@ class RenderProperTies extends Component {
                 <div>
                     <label key={"ten_chi_so"} className="fullwidth">
                         {"Nhập tên chỉ số"}
-                        <input type="text" id={"ten_chi_so"} key={"ten_chi_so"} name={"ten_chi_so"} value={this.props.stateValues.ten_chi_so} className="pricefactor--input" />
+                        <input type="text" id={"ten_chi_so"} key={"ten_chi_so"} name={"ten_chi_so"} value={this.props.stateValues.ten_chi_so} onChange={this.props.OnChangeInput} className="pricefactor--input" />
                     </label>
                 </div>
                 <div>
                     <label key={"ma_gia"} className="fullwidth">
                         {"Mã giá áp dụng"}
-                        <select name={"ma_gia"} id={"ma_gia"} key={"ma_gia"} value={this.props.stateValues.ma_gia} className="pricefactor--select">
+                        <select name={"ma_gia"} id={"ma_gia"} key={"ma_gia"} value={this.props.stateValues.ma_gia} onChange={this.props.OnChangeSelect} className="pricefactor--select">
                             <option value={"VIPHOME1"} >{"VIPHOME1"}</option>
                             <option value={"VIPHOME2"} >{"VIPHOME2"}</option>
                         </select>
@@ -35,13 +35,13 @@ class RenderProperTies extends Component {
                 <div>
                     <label key={"don_gia_co_ban"} className="fullwidth">
                         {"Đơn giá cơ bản"}
-                        <input type="text" id={"don_gia_co_ban"} key={"don_gia_co_ban"} name={"don_gia_co_ban"} value={this.props.stateValues.don_gia_co_ban} className="pricefactor--input" />
+                        <input type="text" id={"don_gia_co_ban"} key={"don_gia_co_ban"} name={"don_gia_co_ban"} value={this.props.stateValues.don_gia_co_ban} onChange={this.props.OnChangeInput} className="pricefactor--input" />
                     </label>
                 </div>
                 <div>
                     <label key={"khung_gio_ap_dung"} className="fullwidth">
                         {"Khung giờ áp dụng"}
-                        <select name={"khung_gio_ap_dung"} id={"ma_gia"} key={"khung_gio_ap_dung"} value={timeLog} className="pricefactor--select">
+                        <select name={"khung_gio_ap_dung"} id={"ma_gia"} key={"khung_gio_ap_dung"} value={timeLog} onChange={this.props.OnChangeSelect} className="pricefactor--select">
                             <option value={"2h-4h"} >{"2h-4h"}</option>
                             <option value={"4h-6h"} >{"4h-6h"}</option>
                         </select>
@@ -51,17 +51,17 @@ class RenderProperTies extends Component {
                     <div className="float-left">
                         <label key={"tinh"} className="fullwidth">
                             {"Áp dụng cho tỉnh thành"}
-                            <input type="text" id={"tinh"} key={"tinh"} name={"tinh"} value={this.props.stateValues.tinh} className="pricefactor--input inline" />
+                            <input type="text" id={"tinh"} key={"tinh"} name={"tinh"} value={this.props.stateValues.tinh} onChange={this.props.OnChangeInput} className="pricefactor--input inline" />
                         </label>
                     </div>
                     <div className="float-left">
                         <label key={"quan_huyen"} className="fullwidth">
                             {"Áp dụng cho quận huyện"}
-                            <input type="text" id={"quan_huyen"} key={"quan_huyen"} name={"quan_huyen"} value={this.props.stateValues.quan_huyen} className="pricefactor--input inline" />
+                            <input type="text" id={"quan_huyen"} key={"quan_huyen"} name={"quan_huyen"} value={this.props.stateValues.quan_huyen} onChange={this.props.OnChangeInput} className="pricefactor--input inline" />
                         </label>
                     </div>
                 </div>
-                <div key={"loai_gia_tri_tang_them"} name={"loai_gia_tri_tang_them"}>
+                <div key={"loai_gia_tri_tang_them"} name={"loai_gia_tri_tang_them"} onChange={this.props.OnChangeRadioButton}>
                     <label className="fullwidth">{"Loại giá trị tăng thêm"}</label>
                     <div className="pricefactor-radio">
                         <input type="radio" value={1} name={"loai_gia_tri_tang_them"} defaultChecked />
@@ -75,13 +75,13 @@ class RenderProperTies extends Component {
                 <div>
                     <label key={"phan_tram_tang_giam"} className="fullwidth">
                         {"Phần trăm tăng/giảm (%)"}
-                        <input type="text" id={"phan_tram_tang_giam"} key={"phan_tram_tang_giam"} value={this.props.stateValues.phan_tram_tang_giam} name={"phan_tram_tang_giam"} className="pricefactor--input" />
+                        <input type="text" id={"phan_tram_tang_giam"} key={"phan_tram_tang_giam"} value={this.props.stateValues.phan_tram_tang_giam} name={"phan_tram_tang_giam"} onChange={this.props.OnChangeInput} className="pricefactor--input" />
                     </label>
                 </div>
                 <div>
                     <label key={"gia_tri_thuc"} className="fullwidth">
                         {"Tổng tiền"}
-                        <input type="text" id={"gia_tri_thuc"} key={"gia_tri_thuc"} name={"gia_tri_thuc"} className="pricefactor--input" />
+                        <input type="text" id={"gia_tri_thuc"} key={"gia_tri_thuc"} name={"gia_tri_thuc"} onChange={this.props.OnChangeInput} className="pricefactor--input" />
                     </label>
                 </div>
                 <div className="submit">
@@ -97,6 +97,31 @@ class PriceFactorCreatorUpdaterForm extends Component {
     constructor(props) {
         super(props);
     }
+    // UpdateState={this.handleUpdateState.bind(this)}
+    
+    OnChangeInput(e) {
+        var name = e.target.name;
+        var value = e.target.value;
+        console.log(name + " input " + value);
+        // this.props.handleUpdateState({ [name]: value });
+    }
+
+    OnChangeSelect(e){ 
+        var name = e.target.name;
+        var value = e.target.value;
+        console.log(name + " select " + value);
+
+        // this.props.handleUpdateState({ [name]: value });
+    }
+
+    OnChangeRadioButton(e) {
+        var name = e.target.name;
+        var value = e.target.value;
+        console.log(name + " radio " + value);
+        // this.props.handleUpdateState({
+        //     [e.target.name]: e.target.value
+        // });
+    }
 
     render() {
         return (
@@ -108,6 +133,10 @@ class PriceFactorCreatorUpdaterForm extends Component {
                         handleSubmit={this.props.handleSubmit}
                         handleClosePopup={this.props.handleClosePopup}
                         stateValues={this.props.stateValues}
+
+                        OnChangeInput={this.OnChangeInput.bind(this)}
+                        OnChangeSelect={this.OnChangeSelect.bind(this)}
+                        OnChangeRadioButton={this.OnChangeRadioButton.bind(this)}
                     />
                 </div>
 
@@ -141,6 +170,10 @@ class PriceFactorCreatorUpdater extends Component {
         }
     }
 
+    handleUpdateState(jsonState) {
+        this.setState(jsonState);
+    }
+
     handleSubmit() {
         console.log("submit");
     }
@@ -155,6 +188,7 @@ class PriceFactorCreatorUpdater extends Component {
                     stateValues={this.state}
                     handleSubmit={this.handleSubmit.bind(this)}
                     handleClosePopup={this.props.closeCreatorUpdaterPopup}
+                    UpdateState={this.handleUpdateState.bind(this)}
                 />
             </div>
         );
