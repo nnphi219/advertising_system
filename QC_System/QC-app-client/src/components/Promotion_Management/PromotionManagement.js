c246be319a100f16337ea05e2c5b957931aea8a9
Phi Nguyen <nnphi219@gmail.com>
2018-03-27 21:29:56 +0700

    Revert "Revert "no message""
    This reverts commit 14bd96601d920ce515272f1c8ce87465d3f1e41c.

---
+++ QC_System/QC-app-client/src/components/Promotion_Management/PromotionManagement.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class PromotionManagement extends Component {
+    render() {
+        return (
+            <div id="page-wrapper">
+                <div className="row">
+                    <div>
+                        <p>
+                            PromotionManagement
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PromotionManagement;
