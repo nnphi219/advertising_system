a1b50479d749ea5ed534f7aa93475c4f73ff48f0
Phi Nguyen <nnphi219@gmail.com>
2018-04-05 23:27:36 +0700

    making todo routes privates

--- QC_System/QC-app-server/api/controllers/todoListController.js
+++ QC_System/QC-app-server/api/controllers/todoListController.js
@@ -2,18 +2,23 @@
 
 var mongoose = require('mongoose'),
   Task = mongoose.model('Tasks');
+const { ObjectID } = require('mongodb');
 
-exports.list_all_tasks = function(req, res) {
-  Task.find({}, function(err, task) {
+exports.list_all_tasks = function (req, res) {
+  Task.find({
+    _creator: req.user._id
+  }, function (err, task) {
     if (err)
       res.send(err);
     res.json(task);
   });
 };
 
-exports.create_a_task = function(req, res) {
+exports.create_a_task = function (req, res) {
   var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
+  new_task._creator = req.user._id;
+
+  new_task.save(function (err, task) {
     if (err)
       res.send(err);
     res.json(task);
@@ -21,32 +26,67 @@ exports.create_a_task = function(req, res) {
 };
 
 
-exports.read_a_task = function(req, res) {
-  Task.findById(req.params.taskId, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
+exports.read_a_task = function (req, res) {
+  var id = req.params.taskId;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Task.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((task) => {
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send({ task });
+  }).catch((e) => {
+    res.status(400).send();
   });
 };
 
 
-exports.update_a_task = function(req, res) {
-  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
+exports.update_a_task = function (req, res) {
+  var id = req.params.taskId;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Task.findOneAndUpdate({
+    _id: id,
+    _creator: req.user._id
+  }).then((task) => {
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  }).catch((e) => {
+    return res.status(400).send();
   });
 };
 
 
-exports.delete_a_task = function(req, res) {
+exports.delete_a_task = function (req, res) {
+  var id = req.params.taskId;
 
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-  Task.remove({
-    _id: req.params.taskId
-  }, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'successfully deleted' });
+  Task.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  }).then((task) => {
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  }).catch((e) => {
+    res.status(400).send();
   });
 };
