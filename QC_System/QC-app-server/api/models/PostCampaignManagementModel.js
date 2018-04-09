b6bb704055989aba837434104e8dc5bcd339f8dc
Tuan Huu Phan <phanhuutuan.01.01.1995@gmail.com>
2018-04-09 17:22:13 +0700

    Refactor post campaign management

--- QC_System/QC-app-server/api/models/PostCampaignManagementModel.js
+++ QC_System/QC-app-server/api/models/PostCampaignManagementModel.js
@@ -6,35 +6,33 @@ var Schema = mongoose.Schema;
 var PostCampaignManagementSchema = new Schema({
     ma_chien_dich: String,
     ma_bai_dang: String,
-    ma_khuyen_mai: String,
+    loai_dich_vu: String, //Lay tu vung quang cao
+    trang_hien_thi: String,
     co_che_hien_thi: String,
     tinh_gia_theo: String,
-    
-    loai_nhan_to:  {// (thời lượng, khung giờ, vị trí)
-        thoi_luong: Number,
-        khung_gio: {
-            bat_dau: Number,
-            ket_thuc: Number
-        },
-        vi_tri: {
-            tinh: String,
-            quan_huyen: String
-        }
+    vi_tri: {
+        tinh: String,
+        quan_huyen: String
+    },
+    khung_gio_hien_thi: {
+        bat_dau: Number,
+        ket_thuc: Number
+    },
+    ngay_bat_dau: {
+        day: Number,
+        month: Number,
+        year: Number
+    },
+    ngay_ket_thuc: {
+        day: Number,
+        month: Number,
+        year: Number
     },
     don_gia_co_ban: Number,
-    start_date: {
-        day: Number,
-        month: Number,
-        year: Number
-    },
-    end_date: {
-        day: Number,
-        month: Number,
-        year: Number
-    },
     thanh_tien: Number,
+    ma_khuyen_mai: String,
     tong_cong: Number,
-    trang_thai: Number,
+    dang_kich_hoat: Number,
     created_date: {
         type: Date,
         default: Date.now
