f9a64a8cb767f706459b6bbcd047773cf72bc51e
Tuan Huu Phan <phanhuutuan.01.01.1995@gmail.com>
2018-05-06 12:30:43 +0700

    Add get marketing post api for front end x

--- X_System/X-app-server/api/routes/QCInteractionRoute.js
+++ X_System/X-app-server/api/routes/QCInteractionRoute.js
@@ -1,6 +1,7 @@
 'use strict';
 var { authenticateQCSystem } = require('../../middleware/authenticate');
 const _ = require('lodash');
+const rp = require('request-promise');
 
 
 module.exports = function (app) {
@@ -20,4 +21,54 @@ module.exports = function (app) {
 
             res.send(resValue);
         });
+
+    app.route('/getPostsBasicOnAppliedPage')
+    .get((req, res) => {
+        let resExample = {
+            vung_trai_1: {
+                type: 'banner',
+                content: {
+                    banner_type: "image",
+                    resource_url:"http://localhost:8080/uploads/20180307092548-7fa8.jpg",
+                    link_page_url: "http://localhost:8080/uploads/20180307092548-7fa8.jpg"
+                }
+            },
+            vung_trai_2: {
+                type: 'banner',
+                content: {
+                    banner_type: "image",
+                    resource_url:"http://localhost:8080/uploads/20180424160229-6f91.jpg",
+                    link_page_url: "http://localhost:8080/uploads/20180424160229-6f91.jpg"
+                }
+            },
+            vung_phai_1: {
+                type: 'banner',
+                content: {
+                    banner_type: "image",
+                    resource_url:"http://localhost:8080/uploads/20180426115906-0b71.jpg",
+                    link_page_url: "http://localhost:8080/uploads/20180426115906-0b71.jpg"
+                }
+            },
+            vung_tren_cung: {
+                type: 'tin_rao',
+                content: {
+                    posts:[]
+                }
+            }
+        }
+        var options = {
+            uri: 'http://localhost:8081/marketing',
+            json: true // Automatically parses the JSON string in the response
+        };
+         
+        rp(options)
+            .then(function (res) {
+                resExample.vung_tren_cung.content.posts = res
+                res.send(resExample)
+            })
+            .catch(function (err) {
+                res.send(resExample)
+            });
+        
+    });
 };
