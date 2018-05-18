41eb72f235a575dc5db6722b904e6bf3e0024288
Phi Nguyen <phi@geta.no>
2018-05-18 18:11:38 +0700

    working

--- QC_System/QC-app-client/src/components/XSystem/Domain_Url/DomainUrlCreatorUpdater.js
+++ QC_System/QC-app-client/src/components/XSystem/Domain_Url/DomainUrlCreatorUpdater.js
@@ -5,21 +5,47 @@ import { RenderInput } from '../../share/InputsRender';
 
 import './domain_url.css';
 
+var validUrl = require('valid-url');
+
 class RenderProperties extends Component {
     render() {
-        var domain_isReadOnly = this.props.modeAction === 'edit' ? 1 : 0;
+        let props = this.props;
+        let domain_isReadOnly = this.props.modeAction === 'edit' ? 1 : 0;
+        let domains = props.stateValues.domains;
+        let domainTokenFields = domains.map((domain) => {
+            return (
+                <div key={domain} className="token domain--token">
+                    <span className="token-label" style={{ maxWidth: "769px" }}>{domain}</span>
+                    <a name={domain} className="close" tabIndex="-1" onClick={props.OnRemoveTokenField}>×</a>
+                </div>
+            );
+        });
 
         return (
-            <div style={{ paddingLeft: "30px" }}>
-                <RenderInput
-                    nameId={"domain"}
-                    title={"Tên domain"}
-                    value={this.props.stateValues.domain}
-                    type={"text"}
-                    className={"user--input"}
-                    errorTitle={this.props.stateValues.error_domain}
-                    OnChangeInput={this.props.OnChangeInput}
-                />
+            <div>
+                <div className="domain">
+                    <div>
+                        <RenderInput
+                            nameId={"domain"}
+                            title={"Tên domain"}
+                            value={this.props.stateValues.domain}
+                            type={"text"}
+                            className={"domain_user--input"}
+                            errorTitle={this.props.stateValues.error_domain}
+                            OnChangeInput={this.props.OnChangeInput}
+                        />
+                    </div>
+                    <div className="domain_tokenfield">
+                        <button type="button" className="btn timeslot_button" onClick={props.OnAddTokenField}>Thêm</button>
+                    </div>
+                    <div className="domain_user--tokenfield tokenfield div_property_margin_bottom">
+                        {domainTokenFields}
+                    </div>
+                </div>
+                <div className="domain_submit">
+                    <button className="btn btn-primary" onClick={this.props.handleSubmit}>Lưu</button>
+                    <button className="btn btn-primary" onClick={this.handleCancel}>Hủy</button>
+                </div>
             </div>
         );
     }
@@ -49,18 +75,16 @@ class DomainUrlCreatorUpdaterForm extends Component {
     }
 
     render() {
+        let props = this.props;
         return (
             <div id="page-wrapper">
-                <h1 className="post_type_title">{this.props.titleForm}</h1>
+                <h1 className="domain_title">{this.props.titleForm}</h1>
                 <RenderProperties
                     OnChangeInput={this.OnChangeInput}
                     modeAction={this.props.modeAction}
                     stateValues={this.props.stateValues}
+                    OnAddTokenField={props.OnAddTokenField}
                 />
-                <div className="submit">
-                    <button className="btn btn-primary" onClick={this.props.handleSubmit}>Lưu</button>
-                    <button className="btn btn-primary" onClick={this.handleCancel}>Hủy</button>
-                </div>
             </div>
         );
     }
@@ -70,10 +94,13 @@ class DomainUrlCreatorUpdater extends Component {
     constructor(props) {
         super(props);
 
-        var jsonState = {};
+        var jsonState = {
+            domains: []
+        };
         this.SetInitState(jsonState);
         this.state = jsonState;
-
+        this.OnAddTokenField = this.OnAddTokenField.bind(this);
+        this.OnRemoveTokenField = this.OnRemoveTokenField.bind(this);
     }
 
     SetInitState(jsonState) {
@@ -201,7 +228,46 @@ class DomainUrlCreatorUpdater extends Component {
         }
     }
 
+    OnAddTokenField(e) {
+        let stateValues = this.state;
+        let currentInputDomain = stateValues.domain;
+        let currentDomains = stateValues.domains;
+
+        if (!validUrl.isUri(currentInputDomain)) {
+            e.preventDefault();
+            return;
+        }
+
+        currentDomains.push(currentInputDomain);
+        let jsonNewState = {
+            domains: currentDomains
+        };
+        this.props.UpdateState(jsonNewState);
+    }
+
+    OnRemoveTokenField(e) {
+        // var stateValues = this.props.stateValues;
+        // var removedTimeSlot = e.target.name;
+        // var selectedTimeSlots = stateValues.selectedTimeSlots.slice();
+        // var array_khung_gio = stateValues.array_khung_gio.slice();
+        // var jsonState = {};
+
+        // if (removedTimeSlot === "all") {
+        //     jsonState.allowAddTimeSlot = true;
+        // }
+
+        // ArrayRemoveItem(selectedTimeSlots, removedTimeSlot);
+        // var remainingTimeSlots = GetRemainingTimeSlots(array_khung_gio, selectedTimeSlots);
+
+        // jsonState.selectedTimeSlots = selectedTimeSlots;
+        // jsonState.remainingTimeSlots = remainingTimeSlots;
+        // jsonState.time_slot = remainingTimeSlots.length > 0 ? remainingTimeSlots[0] : null;
+
+        // this.props.UpdateState(jsonState);
+    }
+
     render() {
+        console.log(this.state);
         var titleForm = this.props.modeAction === "create" ? "Tạo domain" : "Chỉnh loại domain";
         return (
             <div className='div_createform'>
@@ -211,6 +277,7 @@ class DomainUrlCreatorUpdater extends Component {
                     handleSubmit={this.handleSubmit.bind(this)}
                     UpdateState={this.handleUpdateState.bind(this)}
                     modeAction={this.props.modeAction}
+                    OnAddTokenField={this.OnAddTokenField}
                 />
             </div>
         );
